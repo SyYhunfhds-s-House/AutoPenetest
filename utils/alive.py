f36718928a59c29b0e72fc4ca03f54fdf7d2c788
SyYhunfhds <SY1438439330@163.com>
2025-07-15 11:17:12 +0800

    [ignore]: 把暂存区也提交一下

---
+++ utils/alive.py
@@ -0,0 +1,9 @@
+from query import *
+from tqdm import tqdm
+import httpx
+# 导入线程池
+from concurrent.futures import ThreadPoolExecutor, as_completed
+
+# TODO 联动query.py，编写资产探活模块
+def alive_check():
+    pass
